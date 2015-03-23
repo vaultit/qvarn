b4760aa3478b39b28a136d522eda02e5889634a3
Lars Wirzenius <lars.wirzenius@tilaajavastuu.fi>
2015-03-23 14:51:12 +0200

    Add mockup API implementation from dev-team-docs
    This is a placeholder, while we test the Continous Delivery system. We
    need a project that looks real, and allows the API test suite to pass.
    The mockup implementation from the API documentation works for this.
    
    Once we start developing the system for real, the mockup will be
    replaced.
    
    Also include a copy of bottle.py, renamed to unifiedapibottle.py,
    because we want version 0.12 of bottle.py, and Debian wheezy only
    provides 0.10.
    
    Also include a setup.py, which is needed for installation and Debian
    packaging.

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# Copyright (C) 2015  Suomen Tilaajavastuu Oy
+
+
+from distutils.core import setup
+
+
+setup(
+    name='unified-api-backend',
+    version='0.1',
+    description='Unified API backend',
+    author='Suomen Tilaajavastuu Oy',
+    author_email='tilaajavastuu.hifi@tilaajavastuu.fi',
+    scripts=['unified-api-backend'],
+    py_modules=['unifiedapibottle'],
+)
