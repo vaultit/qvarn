c2fcb479fbe229631d3e129ec3f343d0090432a2
Heikki Haapala <heikki.haapala@taitounited.fi>
2015-08-07 14:52:17 +0300

    Add auth information to logging

--- unifiedapi/logging_plugin.py
+++ unifiedapi/logging_plugin.py
@@ -7,8 +7,8 @@ class LoggingPlugin(object):
 
     def apply(self, callback, route):
         def wrapper(*args, **kwargs):
-            self._log_request()
             data = callback(*args, **kwargs)
+            self._log_request()
             self._log_response(data)
             return data
         return wrapper
@@ -21,6 +21,9 @@ class LoggingPlugin(object):
         logging.info(u'Request headers: %r', dict(r.headers))
         if r.method in ('POST', 'PUT') and r.json:
             logging.info(u'Request body (JSON): %r', r.json)
+        logging.info(u'Request authorization client scopes: %r', bottle.request.environ.get(u'scopes'))
+        logging.info(u'Request authorization client id: %r', bottle.request.environ.get(u'client_id'))
+        logging.info(u'Request authorization user id: %r', bottle.request.environ.get(u'user_id'))
 
     def _log_response(self, data):
         r = bottle.response
