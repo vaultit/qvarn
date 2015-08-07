80d784d1832efb3e578a8e747e6b956bc96efde6
Heikki Haapala <heikki.haapala@taitounited.fi>
2015-08-07 17:05:52 +0300

    Change authorization tests to use RSA keys

--- unifiedapi/authorization_validator_tests.py
+++ unifiedapi/authorization_validator_tests.py
@@ -27,6 +27,43 @@ def get_valid_token():
     }
 
 
+private_test_key = '''-----BEGIN RSA PRIVATE KEY-----
+MIIEpQIBAAKCAQEApbW6bK3hmCGyBAM0RHd7nZMn+/NHOzsd4jJoOcSeJIk9SPnT
+m/l/2ycUEEuqyt5hoyABjofZJV8S3RHk/fCaHeb0mvKokhoTXQQKTHOWvUyKKtqV
+6YGmwwKD6WEyPSJbEtBVElr5j/YTYco/D0sIrbhUMVSpCIQT/I1ytGOlnLfqHM20
+HSQusgtRjkUWkaDCBFHebY5C7CpnHNyd5ApeQC2MiZDSpi67GraMJ4ttbimWRUYA
+dsYU11lLlPdpXgwiTeDz8pAsYSajXQVX7WBWnWC11JYMBXcu480eavDNdBTHt++Z
+lcWYutGoAK5XTWhRg46jRVOmmHumj3yNh9W+xwIDAQABAoIBAQCX62KNTmB7a7Db
+cuCRQIVI8md+2gtc5xa/kHzzMSnWzycrZza0UWoBTfNb+TMMqBIVTjt/I1ZVp7MQ
+j95DXTi930YzY/Jdd6B270RN0M7Kn4gwP5OerylmsUCkTmKTn5KlTfAgUt1nOS+N
+wLBNYfoD4fD2BOqvDv+P01HsxUpIwO3xEKp6TvkumnbXgDjgyVrGFoYCgSu/7xuX
+KnqSRz2KvvukAP0ZkLrT69oCUodv4Ya336yjEGgD7YFlvkHjEl97kwr5ziNuoCer
+04iLsKRV2Lj+4oy4HKFdxaGAiENLR4VNrhZDINMBKCe/vgpDXfN6jUCuxdb7ndN9
+2l+9eZxpAoGBANVtHQ2fNs/jyaWdvh42BJycZbmcC5xZ78ipSzDZ6IqLzTd85zeW
+0a4Roxv4viZncp/WuV1hYigE3uv+qpTLSEzdANfY3SXUUv0q+undcO8huaFNdpgF
+DpkQD/uwFxkYdNiD/FWCP9BRUSQM4k468S38OEe93PX1QiTmMcQBfOvdAoGBAMbD
+6ufUucu9CB+t6OcDICAboLXvmWKTzUOUraENRwLxn2cdfWmbbKBqxK6xHpusWjYp
+qwS4Y+hfXM1flbIGirYjIqkjCdeSY4naVyOo4QqCyPIK9RXwHjysSLi/Kc7LL3KP
+yzcEHYIm7UAxlm9VTIgefUhvwA2SVfNxPOykiYzzAoGBAJMS0BSVBQaZqFmyrFLR
+UrhBpnATsoSaDX0v/Jq7b14aHN8B+av7CJ91k/swnIiGfRzcsXxCIYwGX0AtjItg
+0n/1RCF6Vls9R7sipSoH6U1A5lTbtr/nrDmaMgl1PVWT3uFdgsPCMAt0HgBDyKe0
+QoM37eiyU9RCoMQgxWaWx+kZAoGBAMavuxYo/9yYRhGcv06FQky2MU0Mh/ARPMNM
+UM/HvO9FZokl4mJ5ufkVISxa4vTMMZUoy8o5I615/gNRhArkHS56KsCVxNXXgGah
+ei+sNeBS4dmJeHqIf0E5GqyKcplDZFeJQ6LoGzMqBEkCCJWb15fNmoCZLIqkeASU
+ckk/JDxfAoGAHUjRwU36XhH4wKrGd71avRc1nx6OGVuwzyiiZEEaaLPrXg8gJBaR
+ec1V6k23+H/CRrzMFGZCIbMNQaxhPxfCaBINHygcZQ4jsIepFxmcDV++Fi/jMHMf
+cImjZ9dxwWFeouQhOHalSyCiWWYgHSWw3r4jj/L/h2VjchzMOvJQt6A=
+-----END RSA PRIVATE KEY-----'''
+
+
+public_test_key = '''ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCltbpsreGYIbIEAzREd3
+udkyf780c7Ox3iMmg5xJ4kiT1I+dOb+X/bJxQQS6rK3mGjIAGOh9klXxLdEeT98Jod5vSa8qiSGhNdB
+ApMc5a9TIoq2pXpgabDAoPpYTI9IlsS0FUSWvmP9hNhyj8PSwituFQxVKkIhBP8jXK0Y6Wct+oczbQd
+JC6yC1GORRaRoMIEUd5tjkLsKmcc3J3kCl5ALYyJkNKmLrsatowni21uKZZFRgB2xhTXWUuU92leDCJ
+N4PPykCxhJqNdBVftYFadYLXUlgwFdy7jzR5q8M10FMe375mVxZi60agArldNaFGDjqNFU6aYe6aPfI
+2H1b7H'''
+
+
 class AuthorizationValidatorTests(unittest.TestCase):
 
     def setUp(self):
@@ -49,12 +86,11 @@ class AuthorizationValidatorTests(unittest.TestCase):
         self.assertEqual(token, 'tokentoken')
 
     def test_token_validation_with_valid_token(self):
-        secret = u'secret'
         token = get_valid_token()
-        encoded_token = jwt.encode(token, secret, algorithm='HS512')
+        encoded_token = jwt.encode(token, private_test_key, algorithm='RS512')
         result = self.authorization_validator.validate_token(
             encoded_token,
-            secret,
+            public_test_key,
             token[u'iss'])
         self.assertEqual(
             result,
@@ -70,22 +106,20 @@ class AuthorizationValidatorTests(unittest.TestCase):
             })
 
     def test_token_validation_with_invalid_issuer(self):
-        secret = u'secret'
         token = get_valid_token()
-        encoded_token = jwt.encode(token, secret, algorithm='HS512')
+        encoded_token = jwt.encode(token, private_test_key, algorithm='RS512')
         with self.assertRaises(unifiedapi.Forbidden):
             self.authorization_validator.validate_token(
                 encoded_token,
-                secret,
+                public_test_key,
                 u'otherissuer')
 
     def test_token_validation_with_missing_subject(self):
-        secret = u'secret'
         token = get_valid_token()
         del token[u'sub']
-        encoded_token = jwt.encode(token, secret, algorithm='HS512')
+        encoded_token = jwt.encode(token, private_test_key, algorithm='RS512')
         with self.assertRaises(unifiedapi.Forbidden):
             self.authorization_validator.validate_token(
                 encoded_token,
-                secret,
+                public_test_key,
                 token[u'iss'])
