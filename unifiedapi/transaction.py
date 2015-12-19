d9a534444a82354936fd04461385a080a398851d
Lars Wirzenius <lars.wirzenius@tilaajavastuu.fi>
2015-12-19 19:18:57 +0000

    Log transaction, query durations, results counts

--- unifiedapi/transaction.py
+++ unifiedapi/transaction.py
@@ -5,6 +5,7 @@
 
 
 import logging
+import time
 
 
 class Transaction(object):
@@ -12,6 +13,7 @@ class Transaction(object):
     def __init__(self):
         self._sql = None
         self._conn = None
+        self._started = None
 
     def set_sql(self, sql):
         self._sql = sql
@@ -19,22 +21,31 @@ class Transaction(object):
     def __enter__(self):
         assert self._sql is not None
         assert self._conn is None
+        assert self._started is None
+        self._started = time.time()
         self._conn = self._sql.get_conn()
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         assert self._conn is not None
+        assert self._started is not None
         if exc_type is None:
             self._conn.commit()
         else:  # pragma: no cover
             self._conn.rollback()
         self._sql.put_conn(self._conn)
+        duration = time.time() - self._started
+        logging.info('Transaction duration: %.3f ms', duration * 1000.0)
         self._conn = None
+        self._started = None
 
     def _execute(self, query, values):
         logging.debug('Executing SQL: %r using %r', query, values)
+        started = time.time()
         c = self._conn.cursor()
         c.execute(query, values)
+        duration = time.time() - started
+        logging.info('SQL query duration: %.3f ms', duration * 1000.0)
         return c
 
     def create_table(self, table_name, column_name_type_pairs):
@@ -54,7 +65,12 @@ class Transaction(object):
         query = self._sql.format_select(
             table_name, column_names, select_conditions)
         cursor = self._execute(query, select_conditions)
-        return self._construct_row_dicts(column_names, cursor)
+        started = time.time()
+        rows = self._construct_row_dicts(column_names, cursor)
+        duration = time.time() - started
+        logging.info('Result is %d rows', len(rows))
+        logging.info('SQL results fetching: %.3f ms', duration * 1000.0)
+        return rows
 
     def _construct_row_dicts(self, column_names, cursor):
         result = []
