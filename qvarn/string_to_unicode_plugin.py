01fcc92370fb96f29559979d4c4b23a1f569237c
Lars Wirzenius <lars.wirzenius@tilaajavastuu.fi>
2016-02-06 15:21:32 +0000

    Add first batch of copyright statement updates

--- qvarn/string_to_unicode_plugin.py
+++ qvarn/string_to_unicode_plugin.py
@@ -1,8 +1,20 @@
 # string_to_unicode_plugin.py - formats path (and other) string arguments given
 #                               to route methods to unicode
 #
-# Copyright 2015 Suomen Tilaajavastuu Oy
-# All rights reserved.
+# Copyright 2015, 2016 Suomen Tilaajavastuu Oy
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
 class StringToUnicodePlugin(object):
