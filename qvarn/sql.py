a8b8bb5a2e10b2735619a2f53d40f7922420e0d2
Lars Wirzenius <liw@qvarnlabs.com>
2016-07-26 13:35:09 +0300

    Drop silly sql.py debugging thing

--- qvarn/sql.py
+++ qvarn/sql.py
@@ -91,8 +91,6 @@ class SqlAdapter(object):
     # override it.
     type_name = {}
 
-    debug = False
-
     def quote(self, name):
         '''Quote a name for SQL.
 
@@ -120,8 +118,6 @@ class SqlAdapter(object):
             self.quote(table_name),
             u', '.join(column_specs),
         )
-        if self.debug:
-            print 'create table sql:', repr(sql)
         return sql
 
     def format_rename_table(self, old_name, new_name):
@@ -281,7 +277,6 @@ class SqliteAdapter(SqlAdapter):
 
     def __init__(self):
         self._conn = sqlite3.connect(u':memory:')
-        self.debug = False
 
     def format_placeholder(self, column_name):
         return ':{}'.format(self.quote(column_name))
